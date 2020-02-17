133b82b32fce93e663e52e63b8babfa8b1bb3268
meri12345 <38717612+meri12345@users.noreply.github.com>
2020-02-17 18:37:46 +0100

    get ingredients from state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,12 +2,20 @@ import React, {Component} from 'react'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+    state={
+        ingredinets:{
+            salad:1,
+            bacon:1,
+            cheese:2,
+            meat:1
+        }
+    }
 
     render(){
 
        return (
            <div>
-               <Burger/>
+               <Burger ingredients={this.state.ingredinets}/>
                <div>Build Controls</div>
            </div>
        ); 
